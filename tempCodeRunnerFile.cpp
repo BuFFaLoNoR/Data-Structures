b9deeb4956eb45e47598674eaa025f71e240b80f
BuFFaLoNoR <theerat.sc@gmail.com>
2024-09-14 02:47:30 +0700

    add

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <map>
+
 using namespace std;
-void list_erase(vector<int> &v, vector<int> &pos)
-{
-    // write your code here
-    vector<int> ans;
-    int it = 0;
-    sort(pos.begin(),pos.end());
-    vector<bool> mark(v.size(),false);
-    for(auto index:pos) {
-        mark[index] = true;
+
+int return_median(vector<int> v) {
+    if (v.size()%2 == 0) {
+        return (v[v.size()/2] + v[(v.size()/2)-1])/2;
+    } else {
+        return (v[v.size()/2]);
     }
-    for (int i=0; i<v.size(); i++) {
-        if (!mark[i]) {
-            ans.push_back(v[i]);
-        }
-    }
-    v = ans;
 }
-int main()
-{
-    std::ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
+
+int main() {
+    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
+    int n,m;
     cin >> n >> m;
-    vector<int> v(n), pos(m);
-    for (int i = 0; i < n; i++)
+    vector<int> v(n);
+    vector<int> check;
+    for (int i=0; i<n; i++) {
         cin >> v[i];
-    for (int i = 0; i < m; i++)
-        cin >> pos[i];
-    list_erase(v, pos);
-    cout << "After call list_erase" << endl
-         << "Size = " << v.size() << endl;
-    for (auto &x : v)
-        cout << x << " ";
-    cout << endl;
+    }
+    for (int i=0; i<=m; i++) {
+        check.push_back(v[i]);
+    }
+    sort(check.begin(), check.end());
+    cout << return_median(check) << " ";
+    for (int i=1; i<v.size()-m; i++) {
+        check.erase(check.begin());
+        check.push_back(v[i+m]);
+        sort(check.begin(), check.end());
+        cout << return_median(check) << " ";
+    }
+
+    return 0;
 }
