ed64a63bab049a2e95ed413c35da57cc823aa8c4
BuFFaLoNoR <theerat.sc@gmail.com>
2024-09-14 02:31:02 +0700

    add

--- Moving_Median.cpp
+++ Moving_Median.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
 int return_median(vector<int> v) {
-    map<int,int> count;
-    vector<pair<int,int>> for_sort;
-    for (int i:v) {
-        count[i]++;
+    if (v.size()%2 == 0) {
+        return (v[v.size()/2] + v[(v.size()/2)-1])/2;
+    } else {
+        return (v[v.size()/2]);
     }
-    for (auto i:count) {
-        for_sort.push_back({i.second,i.first});
-    }
-    sort(for_sort.begin(),for_sort.end());
-    if (*(for_sort.end()-1)).first >  *(for_sort.end()-2).first {
-        return 
-    }
-
 }
-
 
 int main() {
     std::ios_base::sync_with_stdio(false); std::cin.tie(0);
     int n,m;
     cin >> n >> m;
     vector<int> v(n);
-    for (int i=n; i<n; i++) {
-        cin >> v[n];
+    vector<int> check;
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
     }
-
+    for (int i=0; i<=m; i++) {
+        check.push_back(v[i]);
+    }
+    sort(check.begin(),check.end());
+    cout << return_median(check) << " ";
+    for (int i=1; i<v.size()-m; i++) {
+        check.erase(find(check.begin(), check.end(), v[i-1]));
+        check.push_back(v[i+m]);
+        sort(check.begin(), check.end());
+        cout << return_median(check) << " ";
+    }
 
     return 0;
 }
